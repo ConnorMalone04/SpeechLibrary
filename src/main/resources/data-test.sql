4949b3a330bbbac2b24b46bb50a5ecadad92f9eb
connor malone <conmalon@iu.edu>
2026-03-23 19:16:02 +0000

    remove topic from person insert

--- src/main/resources/data-test.sql
+++ src/main/resources/data-test.sql
@@ -1,5 +1,5 @@
 INSERT INTO topic (id, name) VALUES (1, 'AI');
-INSERT INTO person (id, name, topic) VALUES (1, 'Alan Turing', 1);
+INSERT INTO person (id, name) VALUES (1, 'Alan Turing');
 
 INSERT INTO speech (id, title, content, audio_url, person_id, topic_id)
 VALUES (1, 'Test Speech', 'Test content', null, 1, 1);
